6309ae6ac08aefd95a7c23d16f0907f0e9425d9c
lechos <linneasaltel@gmail.com>
2016-02-11 01:17:29 +0000

    initial commit

--- contact.rb
+++ contact.rb
@@ -1,37 +1,82 @@
 require 'csv'
+require 'pry'
+read_contacts = CSV.read('contactlist2.csv')
 
 # Represents a person in an address book.
 class Contact
 
-  attr_accessor :name, :email
+  attr_accessor :name, :email, :id
 
-  def initialize(name, email)
+  def initialize(id, name, email)
+    @id = id
+    @email = email
+    @name = name
     # TODO: Assign parameter values to instance variables.
   end
 
   # Provides functionality for managing a list of Contacts in a database.
   class << self
 
+    @@file_path = 'contactlist2.csv'
+
+    def list_all
+      contacts = CSV.read(@@file_path)
+      puts contacts
+    end
+
     # Returns an Array of Contacts loaded from the database.
-    def all
+    def print_id
+      contacts = CSV.read(@@file_path)
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
-    def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-    end
+    # def create(name, email)
+    #   #binding.pry
+    #   #contact = Contact.new(name, email)
+    #   CSV.open(@@file_path).each do |csv| csv << [contact.name, contact.email]
+    #   end
+    # #   # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    # end
+    #
 
+    
     # Returns the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+    def show(id)
+      print_id.each do |contact|
+        if contact[0] == id
+          puts contact
+        end
+      end
     end
 
     # Returns an array of contacts who match the given term.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+    def search(query)
+      print_id.each do |contact|
+        found = false
+          contact.each do |column|
+          if column.include? query
+            found = true
+            puts contact
+          end
+        end
+      end
     end
 
+    def create(name, email)
+      #take in two parameters, add to array
+      CSV.open(@@file_path) do |csv|
+      csv << name && csv << email
+      # array = []
+      # array.to_s << name && array.to_s << email
+      # CSV << array.to_s
+    end
+    end
   end
-
 end
+# if contact[1] == name
+        #   puts contact
+        # end
+          # if contact[1].include?(query) || contact[2].include?(query)
+          #   puts contact
+          # end
--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,60 @@
 require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
-class ContactList
+class ContactListUi
+  
+  def run(args)
+    case args[0]
+    when 'create' then create
+    when 'list' then Contact.list_all
+    when 'show' then Contact.show(args[1])
+    when 'search' then Contact.search(args[1])
+    else exit
+    end
+  end
 
+  def user_menu
+    puts "\n" + 
+    "Here is a list of available commands:\n" +
+    "\n" + 
+    "create\n" +
+    "list all\n" + 
+    "show\n" +
+    "quit"
+  end
+
+  def create
+    puts "Name: "
+    name = $stdin.gets.chomp
+    puts "Email: "
+    email = $stdin.gets.chomp
+    contact = Contact.create(name, email)
+  end
+
+
+  # def get_user_input
+  #   input = gets.chomp
+  #   if input == "create"    #
+  #     puts "please add name"
+  #     name = gets.chomp
+  #     puts "please add email"
+  #     email = gets.chomp
+  #     elsif input == "list all"
+  #     puts Contact::list_all
+  #     elsif input == "show"
+  #     puts "Please enter ID"
+  #     id = gets.chomp
+  #     puts Contact::show
+  #     else input == "quit"
+  #     puts Contact::quit
+  #   end
+  # end
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
-
 end
+
+ui = ContactListUi.new
+ui.run(ARGV)
+
+
+# ui.user_menu
+# ui.get_user_input
